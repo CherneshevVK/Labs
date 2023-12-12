8664fc91e6d9fe8c5c253cc467d9cd74d2a5d5cc
CherneshevVK <153637987+CherneshevVK@users.noreply.github.com>
2023-12-12 19:47:10 +0300

    Task_2.py

--- Лабораторная работа 2/Task_2.py
+++ Лабораторная работа 2/Task_2.py
@@ -2,7 +2,7 @@ salary=20000
 spend=25000
 money_capital=spend-salary
 increase=0.03
-for i in range(2,10):
+for i in range(1,10):
     spend*=(1+increase)
     money_capital += spend - salary
 print(round(money_capital,2))
