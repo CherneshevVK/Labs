a4c3da02cfde0d1a4ed55491b88794e1a15075ca
CherneshevVK <153637987+CherneshevVK@users.noreply.github.com>
2023-12-18 18:29:48 +0300

    Update Task_3.py

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -1,6 +1,6 @@
-players=['Player1','Player2','Player3','Player4','Player5','Player6']
-Summa=len(players)
-Playersinteam =Summa//2
-team1=players[0:Playersinteam]
-team2=players[Playersinteam:]
-print(team1,team2)
+players=['player_1','player_2','player_3','player_4','player_5','player_6']
+summa=len(players)
+players_in_team =summa//2
+team_1=players[0:players_in_team]
+team_2=players[players_in_team:]
+print(team_1,team_2)
